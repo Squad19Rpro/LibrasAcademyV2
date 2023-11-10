5dd751fdf4ea9fd6afeccd964eb960360a915813
Heitor Pimentel <130229709+heitorpimentel@users.noreply.github.com>
2023-11-10 01:15:33 -0300

    Inserindo arquivos para testes e atualizações

---
+++ scripts/script.js
@@ -0,0 +1,73 @@
+/* function loading(){
+    $('#preloader').css('display','none');
+} */
+/* Preloader */
+function loading() {
+    setTimeout(function() {
+        $('#preloader').css('display', 'none');
+    }, 500)}; //1000 milissegundos = 1 segundo
+
+/* Botão para voltar ao topo */
+    $(document).ready(function(){
+        // Add smooth scrolling to all links
+        $("a").on('click', function(event) {
+            // Make sure this.hash has a value before overriding default behavior
+            if (this.hash !== "") {
+                // Prevent default anchor click behavior
+                event.preventDefault();
+                // Store hash
+                var hash = this.hash;
+                // Using jQuery's animate() method to add smooth page scroll
+                // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 800, function(){
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                });
+            } // End if
+        });
+        //Nav BG and TopBTN function
+        (function scrollFn(){
+            if($(document).scrollTop() >= 1){
+                $("nav").css({"background-color":"#37517ed4"});
+                $(".up-scroll").addClass("display");
+                setTimeout(function(){
+                    $(".up-scroll").addClass("active");
+                },100);
+            };
+            $(document).scroll(function(){
+                if($(this).scrollTop() >= 1){
+                    $("nav").css({"background-color":"#37517ed4"});
+                    $(".up-scroll").addClass("display");
+                    setTimeout(function(){
+                        $(".up-scroll").addClass("active");
+                    },10);
+                }else{
+                    $("nav").css({"background-color":" #37517e"});
+                    $(".up-scroll").removeClass("active");
+                };
+            });
+        })();
+    });
+    /* pesquisa cursos */
+        document.addEventListener('DOMContentLoaded', function() {
+            const cards = document.querySelectorAll('.card-curso');
+            const input = document.querySelector('#input-busca');
+
+            function removeAccents(str) {
+                return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+            }
+
+            input.addEventListener('input', function() {
+                const searchTerm = removeAccents(input.value.toLowerCase());        
+                cards.forEach(card => {
+                    const cardText = removeAccents(card.textContent.toLowerCase());
+                    if (cardText.includes(searchTerm)) {
+                        card.style.display = 'block';
+                    } else {
+                        card.style.display = 'none';
+                    }
+                });
+            });
+        });
